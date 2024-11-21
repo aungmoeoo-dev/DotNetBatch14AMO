a5c277923a0362bdc7d2d1115e2239e15cb4e451
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-11-21 20:28:50 +0630

    third time done

---
+++ DotNetBatch14AMO.ConsoleApp3/AppSettings.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBatch14AMO.ConsoleApp3;
+
+internal class AppSettings
+{
+
+	public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new()
+	{
+		DataSource = ".",
+		InitialCatalog = "TestDB",
+		UserID = "sa",
+		Password = "Aa145156167!",
+		TrustServerCertificate =  true
+	};
+}

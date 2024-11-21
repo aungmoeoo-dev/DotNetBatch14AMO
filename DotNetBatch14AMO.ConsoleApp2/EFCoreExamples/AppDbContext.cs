9ba11728930d2bc95d6eb493ffc63d814647ec61
Aung Moe Oo <aungmoeoo.dev@gmail.com>
2024-11-21 19:53:55 +0630

    second time done

--- DotNetBatch14AMO.ConsoleApp2/EFCoreExamples/AppDbContext.cs
+++ DotNetBatch14AMO.ConsoleApp2/EFCoreExamples/AppDbContext.cs
@@ -1,18 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DotNetBarch14AMO.ConsoleApp1.EFCoreExamples
+namespace DotNetBatch14AMO.ConsoleApp2.EFCoreExamples;
+
+internal class AppDbContext : DbContext
 {
-	internal class AppDbContext : DbContext
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
-		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		{
-			optionsBuilder.UseSqlServer(AppSettings.SqlConnectionStringBuilder.ConnectionString);
-		}
-
-		public DbSet<TBLBlog> Blogs { get; set; }
+		optionsBuilder.UseSqlServer(AppSettings.SqlConnectionStringBuilder.ConnectionString);
 	}
+
+	public DbSet<TBLBlog> Blogs { get; set; }
 }
 
 [Table("TBL_Blog")]
---
+++ DotNetBatch14AMO.ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNetBatch14AMO.ConsoleApp2.AdoDotNetExamples;
+using DotNetBatch14AMO.ConsoleApp2.DapperExamples;
+using DotNetBatch14AMO.ConsoleApp2.EFCoreExamples;
+
+//AdoDotNetExample adoDotNetExample = new();
+//adoDotNetExample.Read();
+//adoDotNetExample.Edit("456A4F62-7181-4054-A8CD-160A51F343C9");
+//adoDotNetExample.Create("AdoDotNet Create Test title", "AdoDotNet Create Test Author", "AdoDotNet Create Test Content");
+//adoDotNetExample.Update("FFFE1C18-22F0-4804-999F-A27377863DCD", "Ado updated title", "Ado updated Author", "Ado updated content");
+//adoDotNetExample.Delete("FFFE1C18-22F0-4804-999F-A27377863DCD");
+
+//DapperExample dapperExample = new();
+//dapperExample.Read();
+//dapperExample.Edit("456A4F62-7181-4054-A8CD-160A51F343C8");
+//dapperExample.Create("Dapper Create Test title", "Dapper Create Test Author", "Dapper Create Test Content");
+//dapperExample.Update("D384129B-6B07-4E59-9B19-1E06EA418A72", "Dapper updated title", "Dapper updated Author", "Dapper updated content");
+//dapperExample.Delete("D384129B-6B07-4E59-9B19-1E06EA418A72");
+
+//EFCoreExample eFCoreExample = new();
+//eFCoreExample.Read();
+//eFCoreExample.Edit("456A4F62-7181-4054-A8CD-160A51F343C9");
+//eFCoreExample.Create("EFCore create test 1", "EFCore test author", "EFCore test content");
+//eFCoreExample.Update("e0c30939-eb87-4301-b676-5d2e025fdfcd", "Updated EFCore test 1", "Updated EFCore test author", "Updated EFCore test content");
+//eFCoreExample.Delete("85926186-29bd-4081-ab3a-dd0b496be06b");
